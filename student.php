b5d485699483154ac7fada26b2e36e8579a54a05
Sudhanshu Shukla <88171846+ssudhanshu37@users.noreply.github.com>
2022-05-31 04:46:09 +0530

    Update student.php

--- student.php
+++ student.php
@@ -18,7 +18,6 @@ if(empty($_SESSION['member_id'])){
 <a href="student.php">Home</a> | <a href="vote.php">Current Polls</a> | <a href="manage-profile.php">Manage My Profile</a> | <a href="changepass.php">Change Password</a>| <a href="logout.php">Logout</a>
 </div>
 <div id="container">
-<p> Click a link above to do some stuff.</p>
 </div>
 <div id="footer">
 <div class="bottom_addr">DataBase Mangement System Project by Aasim Sayyad, Sudhanshu Shukla and Pratik mehta.</div>
